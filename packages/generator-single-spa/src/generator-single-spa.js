8e211901f870d9a765c6beb9498f12fcce8fb118
Joel Denning <joeldenning@gmail.com>
2019-12-18 14:57:38 -0700

    Justin's feedback

--- packages/generator-single-spa/src/generator-single-spa.js
+++ packages/generator-single-spa/src/generator-single-spa.js
@@ -21,7 +21,7 @@ module.exports = class SingleSpaGenerator extends Generator {
         {
           type: 'list',
           name: 'moduleType',
-          message: 'Which kind of in-browser module do you want create-single-spa to create for you?',
+          message: 'Select type to generate',
           choices: [
             { name: 'single-spa application / parcel', value: 'app-parcel' },
             { name: 'in-browser utility module (styleguide, api cache, etc)', value: 'raw-module' },
