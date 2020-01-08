1a73e03fdf495d211fb4f6e93b5a2c04b8da826d
Joel Denning <joeldenning@gmail.com>
2020-01-08 13:27:50 -0700

    Evan's feedback

--- packages/generator-single-spa/src/angular/generator-single-spa-angular.js
+++ packages/generator-single-spa/src/angular/generator-single-spa-angular.js
@@ -2,7 +2,7 @@ const Generator = require('yeoman-generator')
 const { spawn } = require('child_process')
 
 module.exports = class SingleSpaAngularGenerator extends Generator {
-  async runVueCli() {
+  async runAngularCli() {
     spawn('ng', ['new'], { stdio: 'inherit' })
   }
 }
