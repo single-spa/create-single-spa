a7797d41672880967e7f98035d1b6e7d3c124e99
Joel Denning <joeldenning@gmail.com>
2020-01-20 17:15:52 -0700

    Update webpack.config.js

--- packages/generator-single-spa/src/root-config/templates/webpack.config.js
+++ packages/generator-single-spa/src/root-config/templates/webpack.config.js
@@ -21,7 +21,11 @@ module.exports = env => ({
     ]
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': true
+    },
+    disableHostCheck: true
   },
   plugins: [
     new HtmlWebpackPlugin({
