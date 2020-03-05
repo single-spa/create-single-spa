7cb7b08d06717966892db4c4cb3521eb7d9457e3
kshammer <kshammerschmidt@gmail.com>
2020-03-04 22:53:32 -0600

    Revert "Updated root-config.js so that the template will work out of the box."
    This reverts commit 29b1bc12d7d938fd5e5003eead8ef6c25de44a3e.

--- packages/generator-single-spa/src/root-config/templates/src/root-config.js
+++ packages/generator-single-spa/src/root-config/templates/src/root-config.js
@@ -2,8 +2,8 @@ import { registerApplication, start } from "single-spa";
 import * as isActive from "./activity-functions";
 
 registerApplication(
-  "@react-mf/navbar",
-  () => System.import("@react-mf/navbar"),
+  "@<?- orgName ?>/navbar",
+  () => System.import("@<?- orgName ?>/navbar"),
   isActive.navbar
 );
 
