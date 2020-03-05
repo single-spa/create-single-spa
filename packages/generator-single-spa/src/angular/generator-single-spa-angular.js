2ad9eb5b74e6324aa4db7c09bbb50d9b4dc93209
Joel Denning <joeldenning@gmail.com>
2020-03-04 17:00:59 -0700

    Using npx to execute vue and angular clis (#19)

--- packages/generator-single-spa/src/angular/generator-single-spa-angular.js
+++ packages/generator-single-spa/src/angular/generator-single-spa-angular.js
@@ -3,6 +3,6 @@ const { spawn } = require('child_process')
 
 module.exports = class SingleSpaAngularGenerator extends Generator {
   async runAngularCli() {
-    spawn('ng', ['new'], { stdio: 'inherit' })
+    spawn('npx', ['@angular/cli', 'new'], { stdio: 'inherit' })
   }
 }
--- packages/generator-single-spa/src/vue/generator-single-spa-vue.js
+++ packages/generator-single-spa/src/vue/generator-single-spa-vue.js
@@ -3,6 +3,6 @@ const { spawn } = require('child_process')
 
 module.exports = class SingleSpaVueGenerator extends Generator {
   async runVueCli() {
-    spawn('vue', ['create', '.'], { stdio: 'inherit' })
+    spawn('npx', ['@vue/cli', 'create', '.'], { stdio: 'inherit' })
   }
 }
