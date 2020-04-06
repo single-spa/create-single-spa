8cab778a4e57d67e3d48c8c4a7ac4eedbd161e19
Justin McMurdie <mcmurdiej@gmail.com>
2020-04-06 15:37:09 -0600

    running prettier

--- packages/generator-single-spa/src/react/templates/src/set-public-path.js
+++ packages/generator-single-spa/src/react/templates/src/set-public-path.js
@@ -5,4 +5,4 @@ import { setPublicPath } from "systemjs-webpack-interop";
  * https://single-spa.js.org/docs/faq/#code-splits
  */
 
-setPublicPath('@<%= orgName %>/<%= projectName %>');
+setPublicPath("@<%= orgName %>/<%= projectName %>");
