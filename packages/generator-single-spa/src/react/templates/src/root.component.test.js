0c50ddec2dc9a42f3019af033fc84eee3baa5405
Carlos <filoxo@users.noreply.github.com>
2020-04-10 15:23:52 -0600

    Add jest-dom to React template (#74)
    * Install @testing-library/jest-dom
    
    * Include jest-dom extends in setupFilesAfterEnv
    
    * Add root component test
    
    * Copy root component test from template
    
    * Remove passWithNoTests since a basic test was added
    
    * Set jest rootDir to src/ only
    
    * Make setupFiles relative to rootDir
    
    * Fix fork-ts-checker-webpack-plugin constructor invocation

---
+++ packages/generator-single-spa/src/react/templates/src/root.component.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Root from "./root.component";
+
+describe("Root component", () => {
+  it("should be in the document", () => {
+    const { getByText } = render(<Root name="Testapp" />);
+    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+  });
+});
