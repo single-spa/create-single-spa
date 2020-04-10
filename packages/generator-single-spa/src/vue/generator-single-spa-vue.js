4a7bce1f438c2ded756eb46b5eceef06fdf27f0b
Joel Denning <joeldenning@gmail.com>
2020-04-10 16:05:24 -0600

    Customize destination directory and use global vue/angular when possible. Resolves #56.

--- packages/generator-single-spa/src/vue/generator-single-spa-vue.js
+++ packages/generator-single-spa/src/vue/generator-single-spa-vue.js
@@ -1,9 +1,23 @@
 const Generator = require("yeoman-generator");
 const { spawn } = require("child_process");
+const util = require("util");
+const commandExists = util.promisify(require("command-exists"));
 
 module.exports = class SingleSpaVueGenerator extends Generator {
   async runVueCli() {
-    const npxCmd = process.platform === "win32" ? "npx.cmd" : "npx";
-    spawn(npxCmd, ["@vue/cli", "create", "."], { stdio: "inherit" });
+    const globalInstallation = await commandExists("vue");
+
+    let command,
+      args = [];
+    if (globalInstallation) {
+      command = "vue";
+    } else {
+      command = process.platform === "win32" ? "npx.cmd" : "npx";
+      args.push("@vue/cli");
+    }
+
+    spawn(command, args.concat(["create", this.options.dir || "."]), {
+      stdio: "inherit",
+    });
   }
 };
