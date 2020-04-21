7d1bcb2c0fe5572bb19b173f193c90e84fd5ff67
Joel Denning <joeldenning@gmail.com>
2020-04-21 14:11:00 -0600

    Adding check for minimum nodejs version (#101)
    * Adding check for minimum nodejs version
    
    * Carlos' feedback

--- packages/create-single-spa/bin/create-single-spa.js
+++ packages/create-single-spa/bin/create-single-spa.js
@@ -1,8 +1,42 @@
 #!/usr/bin/env node
 
+checkNodeVersion();
+
 const yeoman = require("yeoman-environment");
 const argv = require("yargs").argv;
 
 const env = yeoman.createEnv();
 env.registerStub(require("generator-single-spa"), "single-spa");
-env.run(`single-spa ${argv._.join(" ")}`, argv);
+env.run("single-spa " + argv._.join(" "), argv);
+
+function checkNodeVersion() {
+  const minVersion = "10.4.0";
+  const [minMajor, minMinor, minPatch] = minVersion.split(".").map(Number);
+  const [
+    currentMajor,
+    currentMinor,
+    currentPatch,
+  ] = process.versions.node.split(".").map(Number);
+
+  let validVersion = true;
+
+  if (currentMajor < minMajor) {
+    validVersion = false;
+  } else if (currentMajor === minMajor) {
+    if (currentMinor < minMinor) {
+      validVersion = false;
+    } else if (currentMinor === minMinor && currentPatch < minPatch) {
+      validVersion = false;
+    }
+  }
+
+  if (!validVersion) {
+    throw Error(
+      "create-single-spa requires NodeJS >= " +
+        minVersion +
+        ", but you are using NodeJS " +
+        process.versions.node +
+        "."
+    );
+  }
+}
