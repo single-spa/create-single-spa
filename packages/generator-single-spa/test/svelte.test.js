c4b82d157fc6854736ee323ec3a8e7a1b8fefc4c
Carlos Filoteo <c.filoteo12@gmail.com>
2020-05-29 08:28:22 -0600

    Add tests for svelte option in cli

---
+++ packages/generator-single-spa/test/svelte.test.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const generator = require("../src/generator-single-spa");
+const helpers = require("yeoman-test");
+const assert = require("yeoman-assert");
+
+describe("generator-single-spa-svelte", () => {
+  let runContext;
+
+  afterEach(() => {
+    runContext.cleanTestDirectory();
+  });
+
+  it("handles yarn option properly", () => {
+    runContext = helpers
+      .run(generator)
+      .withOptions({
+        framework: "svelte",
+      })
+      .withPrompts({
+        packageManager: "yarn",
+      });
+
+    return runContext.then((dir) => {
+      assert.file(path.join(dir, "package.json"));
+    });
+  });
+
+  it("handles npm option properly", () => {
+    runContext = helpers
+      .run(generator)
+      .withOptions({
+        framework: "react",
+      })
+      .withPrompts({
+        packageManager: "npm",
+      });
+
+    return runContext.then((dir) => {
+      assert.file(path.join(dir, "package.json"));
+    });
+  });
+
+  it("copies the correct files over", () => {
+    runContext = helpers
+      .run(generator)
+      .withOptions({
+        framework: "svelte",
+      })
+      .withPrompts({
+        packageManager: "npm",
+      });
+
+    return runContext.then((dir) => {
+      assert.file(path.join(dir, "rollup.config.js"));
+      assert.file(path.join(dir, "src/App.svelte"));
+    });
+  });
+});
