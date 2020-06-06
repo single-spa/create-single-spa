a7dc710bf33a5f7947ff0d4e3e39351735352c1f
Carlos <filoxo@users.noreply.github.com>
2020-06-06 15:41:47 -0600

    Feat/additional console instruction vue (#137)
    * Get packageManager option
    
    * Pass packageManager to 'vue create'
    
    * Add skipGetStarted to prevent extra output
    
    * Format
    
    * Add finished output to instruct users to instant test
    
    * Remove (probably) unneeded encodeURIComponent
    
    * Let vue-cli handle selecting a package manager
    
    * Detect if yarn was used and update command

--- packages/generator-single-spa/src/vue/generator-single-spa-vue.js
+++ packages/generator-single-spa/src/vue/generator-single-spa-vue.js
@@ -2,6 +2,8 @@ const Generator = require("yeoman-generator");
 const { spawnSync } = require("child_process");
 const util = require("util");
 const commandExists = util.promisify(require("command-exists"));
+const chalk = require("chalk");
+const path = require("path");
 
 module.exports = class SingleSpaVueGenerator extends Generator {
   async runVueCli() {
@@ -20,13 +22,13 @@ module.exports = class SingleSpaVueGenerator extends Generator {
       command += ".cmd";
     }
 
-    const cwd = this.options.dir || ".";
+    this.cwd = this.options.dir || ".";
 
     const { status, signal } = spawnSync(
       command,
-      args.concat(["create", cwd]),
+      args.concat(["create", this.cwd, "--skipGetStarted"]),
       {
-        stdio: "inherit"
+        stdio: "inherit",
       }
     );
 
@@ -35,13 +37,28 @@ module.exports = class SingleSpaVueGenerator extends Generator {
     } else if (status !== 0) {
       process.exit(status);
     } else {
+      // We purposely do not attempt to install in one command using presets to avoid being too restrictive with application configuration
       spawnSync(command, args.concat(["add", "single-spa"]), {
         stdio: "inherit",
-        cwd,
+        cwd: this.cwd,
         env: Object.assign({}, process.env, {
-          VUE_CLI_SKIP_DIRTY_GIT_PROMPT: true
-        })
+          VUE_CLI_SKIP_DIRTY_GIT_PROMPT: true,
+        }),
       });
     }
+  }
+
+  async finished() {
+    const usedYarn = this.fs.exists(path.resolve(this.cwd, "yarn.lock"));
+    console.log(
+      chalk.bgWhite.black(
+        `Project setup complete!
+Steps to test your Vue single-spa application:
+1. Run '${usedYarn ? "yarn" : "npm run"} serve'
+2. Go to http://single-spa-playground.org/playground/instant-test?name=${
+          this.options.dir
+        }&url=%2F%2Flocalhost%3A8080%2Fjs%2Fapp.js&framework=vue to see it working!`
+      )
+    );
   }
 };
