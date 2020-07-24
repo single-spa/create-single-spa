d73aafb1b38df0ced8e98016ef5df9c9dfae9ef3
Carlos <filoxo@users.noreply.github.com>
2020-07-24 15:42:51 -0600

    Fix/vue app orgname (#156)
    * Prompt for orgName
    
    * Derive name and dir for vue create command
    
    * Update package.json name with orgName after create before adding single-spa plugin
    
    * Reuse options.dir; update instructions with projectName
    
    * Fix typo
    
    Co-authored-by: Joel Denning <joeldenning@gmail.com>
    
    Co-authored-by: Joel Denning <joeldenning@gmail.com>

--- packages/generator-single-spa/src/vue/generator-single-spa-vue.js
+++ packages/generator-single-spa/src/vue/generator-single-spa-vue.js
@@ -4,8 +4,34 @@ const util = require("util");
 const commandExists = util.promisify(require("command-exists"));
 const chalk = require("chalk");
 const path = require("path");
+const fs = require("fs");
+const isValidName = require("../naming");
 
 module.exports = class SingleSpaVueGenerator extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.option("orgName", {
+      type: String,
+    });
+  }
+  async getOptions() {
+    while (!this.options.orgName) {
+      let { orgName } = await this.prompt([
+        {
+          type: "input",
+          name: "orgName",
+          message: "Organization name (use lowercase and dashes)",
+        },
+      ]);
+
+      orgName = orgName && orgName.trim();
+      if (!orgName) console.log(chalk.red("orgName must be provided!"));
+      if (!isValidName(orgName))
+        console.log(chalk.red("orgName must use lowercase and dashes!"));
+      this.options.orgName = orgName;
+    }
+  }
   async runVueCli() {
     const globalInstallation = await commandExists("vue");
 
@@ -22,12 +48,17 @@ module.exports = class SingleSpaVueGenerator extends Generator {
       command += ".cmd";
     }
 
-    this.cwd = this.options.dir || ".";
+    // Derive projectName value
+    const { dir, name } = path.parse(this.options.dir || ".");
+    if (!name) throw new Error("projectName must be provided!");
+    if (!isValidName(name))
+      throw new Error("projectName must use lowercase and dashes!");
 
     const { status, signal } = spawnSync(
       command,
-      args.concat(["create", this.cwd, "--skipGetStarted"]),
+      args.concat(["create", name, "--skipGetStarted"]),
       {
+        cwd: dir,
         stdio: "inherit",
       }
     );
@@ -36,27 +67,33 @@ module.exports = class SingleSpaVueGenerator extends Generator {
       process.exit(1);
     } else if (status !== 0) {
       process.exit(status);
-    } else {
-      // We purposely do not attempt to install in one command using presets to avoid being too restrictive with application configuration
-      spawnSync(command, args.concat(["add", "single-spa"]), {
-        stdio: "inherit",
-        cwd: this.cwd,
-        env: Object.assign({}, process.env, {
-          VUE_CLI_SKIP_DIRTY_GIT_PROMPT: true,
-        }),
-      });
     }
-  }
 
+    const pkgJsonPath = path.resolve(this.options.dir, "package.json");
+    const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath));
+    this.projectName = pkgJson.name = `@${this.options.orgName}/${name}`;
+    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+
+    // We purposely do not attempt to install in one command using presets to avoid being too restrictive with application configuration
+    spawnSync(command, args.concat(["add", "single-spa"]), {
+      stdio: "inherit",
+      cwd: this.options.dir,
+      env: Object.assign({}, process.env, {
+        VUE_CLI_SKIP_DIRTY_GIT_PROMPT: true,
+      }),
+    });
+  }
   async finished() {
-    const usedYarn = this.fs.exists(path.resolve(this.cwd, "yarn.lock"));
+    const usedYarn = this.fs.exists(
+      path.resolve(this.options.dir, "yarn.lock")
+    );
     console.log(
       chalk.bgWhite.black(
         `Project setup complete!
 Steps to test your Vue single-spa application:
 1. Run '${usedYarn ? "yarn" : "npm run"} serve'
 2. Go to http://single-spa-playground.org/playground/instant-test?name=${
-          this.options.dir
+          this.projectName
         }&url=%2F%2Flocalhost%3A8080%2Fjs%2Fapp.js&framework=vue to see it working!`
       )
     );
