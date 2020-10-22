3b66124edd8f1eb856b2a0358e4548f259879653
Carlos <filoxo@users.noreply.github.com>
2020-10-22 07:59:09 -0600

    Fix Angular generator paths (#208)
    * Use dir as cwd; Remove passing --directory flag to angular-cli
    
    * Lock create-single-spa to generator-single-spa version
    
    * Fix angular-cli silent error if dir does not exist

--- packages/generator-single-spa/src/angular/generator-single-spa-angular.js
+++ packages/generator-single-spa/src/angular/generator-single-spa-angular.js
@@ -2,6 +2,7 @@ const Generator = require("yeoman-generator");
 const { spawnSync } = require("child_process");
 const util = require("util");
 const path = require("path");
+const fs = require("fs");
 const commandExists = util.promisify(require("command-exists"));
 const chalk = require("chalk");
 const validate = require("../validate-naming");
@@ -44,18 +45,19 @@ module.exports = class SingleSpaAngularGenerator extends Generator {
       command += ".cmd";
     }
 
-    const cwd = path.resolve(this.options.dir || ".");
+    const cwd = path.resolve(this.options.dir);
+
+    // `ng new` fails if cwd doesn't already exist
+    if (!fs.existsSync(cwd)) fs.mkdirSync(cwd);
 
     const { status, signal } = spawnSync(
       command,
       args.concat([
         "new",
         this.options.projectName, // name of the new workspace and initial project
-        "--directory",
-        cwd,
         // "--routing", false, TODO: Figure out how to interop with single-spa-angular's routing option so that we don't ask the user twice with opposite defaults
       ]),
-      { stdio: "inherit" }
+      { stdio: "inherit", cwd }
     );
 
     if (signal) {
@@ -65,7 +67,7 @@ module.exports = class SingleSpaAngularGenerator extends Generator {
     } else {
       spawnSync(command, args.concat(["add", "single-spa-angular"]), {
         stdio: "inherit",
-        cwd,
+        cwd: path.resolve(cwd, this.options.projectName),
       });
 
       console.log(
