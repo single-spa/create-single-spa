096576ce5c0fd884a7e13bda9330e85329fc0d62
Joel Denning <joeldenning@gmail.com>
2020-10-22 20:32:39 -0600

    Disabling HtmlWebpackPlugin and StandaloneSingleSpaPlugin in prod. Resolves #209.

--- packages/single-spa-welcome/webpack.config.js
+++ packages/single-spa-welcome/webpack.config.js
@@ -1,11 +1,12 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 
-module.exports = (webpackConfigEnv) => {
+module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "single-spa",
     projectName: "welcome",
     webpackConfigEnv,
+    argv,
   });
 
   const config = webpackMerge.smartStrategy({ externals: "replace" })(
