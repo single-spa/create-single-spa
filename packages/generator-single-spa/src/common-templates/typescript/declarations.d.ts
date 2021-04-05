0145cea80d8dc481e2dae917764ac0e9f37356d3
Pieter Boeren <p.boeren@vicrea.nl>
2021-04-05 10:42:51 +0200

    Added image loading via asset/resource

--- packages/generator-single-spa/src/common-templates/typescript/declarations.d.ts
+++ packages/generator-single-spa/src/common-templates/typescript/declarations.d.ts
@@ -2,3 +2,44 @@ declare module "*.html" {
   const rawHtmlFile: string;
   export = rawHtmlFile;
 }
+
+declare module "*.bmp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
