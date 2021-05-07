8f1c94fcff087ad45a0fd44dc006c826e6e901a5
gjhommersom <gjhommersom@hotmail.com>
2021-05-08 00:30:13 +0200

    fixed generated jest config for react unable to load parcels (#292)
    Co-authored-by: Gert-Jan Hommersom <g.hommersom@flexposure.nl>

---
+++ .changeset/seven-windows-attend.md
@@ -0,0 +1,5 @@
+---
+"generator-single-spa": patch
+---
+
+fixed generated jest config for react unable to load parcels
--- packages/generator-single-spa/src/react/templates/jest.config.js
+++ packages/generator-single-spa/src/react/templates/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
+    "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
