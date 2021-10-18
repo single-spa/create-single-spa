78d1520b4fabf3a81a8985a568839cbc0723134b
Joel Denning <joeldenning@gmail.com>
2021-10-18 17:17:39 -0600

    Control single-spa-layout version. Use version 1. Resolves #129.

--- scripts/upgrade-dependencies.sh
+++ scripts/upgrade-dependencies.sh
@@ -16,5 +16,6 @@ pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/common-templates/
 pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/react/templates/react.package.json
 pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/react/templates/typescript/typescript-react.package.json
 pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/root-config/templates/root-config.package.json
+pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/root-config/templates/root-config-layout.package.json
 pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/svelte/templates/svelte.package.json
 pnpx ncu -iu --packageFile ./packages/generator-single-spa/src/util-module/templates/util-module.package.json
