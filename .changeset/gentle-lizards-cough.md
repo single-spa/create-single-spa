ef46e40874e694556ab02a14802131cb313b4c1a
Joel Denning <joeldenning@gmail.com>
2024-12-19 11:42:10 -0700

    Upgrade single-spa-welcome to React 19 (#432)
    * Upgrade single-spa-welcome to React 19
    
    * Fix tests
    
    * Fix tests
    
    * Try to fix tests
    
    * Upgrade pnpm
    
    * Try switching to native fetch
    
    * Changeset
    
    * Fix tests
    
    * Fix lint

---
+++ .changeset/gentle-lizards-cough.md
@@ -0,0 +1,5 @@
+---
+"generator-single-spa": patch
+---
+
+Fix typescript error in no-layout root configs
---
+++ packages/single-spa-welcome/eslint.config.mjs
@@ -0,0 +1,22 @@
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends("react-important-stuff", "plugin:prettier/recommended"),
+  {
+    languageOptions: {
+      parser: babelParser,
+    },
+  },
+];
