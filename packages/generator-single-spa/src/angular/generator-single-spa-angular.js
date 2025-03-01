5fa129a9d3fc06e4da4b21b1051213f40a257cb4
Arman Ossi Loko <armanossiloko@outlook.com>
2025-03-02 00:24:08 +0100

    Added more console logs for failed Angular scaffolding #449

--- packages/generator-single-spa/src/angular/generator-single-spa-angular.js
+++ packages/generator-single-spa/src/angular/generator-single-spa-angular.js
@@ -50,7 +50,7 @@ module.exports = class SingleSpaAngularGenerator extends Generator {
     // `ng new` fails if cwd doesn't already exist
     if (!fs.existsSync(cwd)) fs.mkdirSync(cwd);
 
-    const { status, signal } = spawnSync(
+    const { status, signal, error } = spawnSync(
       command,
       args.concat([
         "new",
@@ -60,9 +60,21 @@ module.exports = class SingleSpaAngularGenerator extends Generator {
       { stdio: "inherit", cwd }
     );
 
+    if (error) {
+      if (error.message) {
+        console.error("The process encountered an error: ", error.message);
+      } else {
+        console.error("The process encountered an error: ", error);
+      }
+
+      process.exit(1);
+    }
+
     if (signal) {
+      console.error(`The process was terminated by signal: ${signal}`);
       process.exit(1);
     } else if (status !== 0) {
+      console.error(`The process exited with a status: ${status}`);
       process.exit(status);
     } else {
       spawnSync(command, args.concat(["add", "single-spa-angular"]), {
